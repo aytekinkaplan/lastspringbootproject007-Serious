e9df9c3a3294929685f9624614d9ba1f48f869c8
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-08 20:19:02 +0300

    Initial commit

---
+++ src/main/java/com/springboot/lastspringbootproject007/Lastspringbootproject007Application.java
@@ -0,0 +1,13 @@
+package com.springboot.lastspringbootproject007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lastspringbootproject007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lastspringbootproject007Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/lastspringbootproject007/Lastspringbootproject007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.lastspringbootproject007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lastspringbootproject007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
